b6e69ffbc76f67d5271b65627ed287ff66cbf2b3
Greg Greenfield <Greg.Greenfield@intergen.co.nz>
2019-03-11 21:15:10 +1300

    initial setup

---
+++ Yatzee/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { DieComponent } from './die/die.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DieComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
