1e81f90c1fd3309071b93ed95cc526f419a5a9c9
Greg Greenfield <greg.greenfield@edmi.co.nz>
2019-03-12 09:28:09 +1300

    adding to the score card display

--- Yatzee/src/app/scorecards/scorecards.component.ts
+++ Yatzee/src/app/scorecards/scorecards.component.ts
@@ -21,6 +21,37 @@ export class ScorecardsComponent implements OnInit {
   get displayDiceNumbers(){
     return this.dice.map(d => d.currentSelected);
   }
+
+  get numbersSubTotalUpper(): number{
+    return this.scorecard.ones + 
+          this.scorecard.twos +
+          this.scorecard.threes +
+          this.scorecard.fours +
+          this.scorecard.fives +
+          this.scorecard.sixes;
+  }
+
+  get numbersSubTotalLower(): number {
+    return this.scorecard.threeOfAKind +
+          this.scorecard.fourOfAKind +
+          this.scorecard.fullHouse +
+          this.scorecard.smallStraight +
+          this.scorecard.largeStraight +
+          this.scorecard.chance +
+          (this.scorecard.Yatzee ? this.scorecard.Yatzee.length * 50 : 0);
+  }
+
+  get topSectionBonus(): boolean {
+    return this.numbersSubTotalUpper > 65;
+  }
+
+  get topScore(): number {
+    return this.topSectionBonus ? this.numbersSubTotalUpper + 25 : this.numbersSubTotalUpper;
+  }
+
+  get Total(): number {
+    return this.topScore + this.numbersSubTotalLower;
+  }
 }
 
 export class Scorecard {
